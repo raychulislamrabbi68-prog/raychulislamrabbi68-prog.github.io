9776d660635dbafbfd32e5989c3d101418817455
raychulislamrabbi68-prog <raychulislamrabbi68@gmail.com>
2026-08-01 19:16:31 +0600

    Initialize Firebase in admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-database.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCirzx_QMyloG6ummoNBtRzhrgxfjcvevs",
+  authDomain: "raychul-islam-rabbi-website.firebaseapp.com",
+  databaseURL: "https://raychul-islam-rabbi-website-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "raychul-islam-rabbi-website",
+  storageBucket: "raychul-islam-rabbi-website.firebasestorage.app",
+  messagingSenderId: "1043773920946",
+  appId: "1:1043773920946:web:750dcb149d29f354cddbab"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+console.log("✅ Firebase Connected");
