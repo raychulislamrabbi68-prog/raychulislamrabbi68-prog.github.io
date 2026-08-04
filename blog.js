060e31169c2d53090c33ab65782d536b805e1b79
raychulislamrabbi68-prog <raychulislamrabbi68@gmail.com>
2026-08-04 14:30:03 +0600

    Integrate Firebase for blog data retrieval

--- blog.js
+++ blog.js
@@ -1,1 +1,108 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { getDatabase, ref, get } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-database.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCirzx_QMyloG6ummoNBtRzhrgxfjcvevs",
+  authDomain: "raychul-islam-rabbi-website.firebaseapp.com",
+  databaseURL: "https://raychul-islam-rabbi-website-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "raychul-islam-rabbi-website",
+  storageBucket: "raychul-islam-rabbi-website.firebasestorage.app",
+  messagingSenderId: "1043773920946",
+  appId: "1:1043773920946:web:750dcb149d29f354cddbab"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+const blogContainer = document.getElementById("blog-container");
+
+const banglaBtn = document.getElementById("banglaBtn");
+const englishBtn = document.getElementById("englishBtn");
+
+let allBlogs = [];
+
+
+get(ref(db, "blogs"))
+.then((snapshot)=>{
+
+if(snapshot.exists()){
+
+allBlogs = Object.values(snapshot.val());
+
+showBlogs("bn");
+
+}else{
+
+blogContainer.innerHTML = "<h2>No Blog Found</h2>";
+
+}
+
+})
+.catch((error)=>{
+
+console.log(error);
+
+blogContainer.innerHTML = "<h2>Error Loading Blog</h2>";
+
+});
+
+
+
+function showBlogs(language){
+
+blogContainer.innerHTML = "";
+
+
+allBlogs.forEach((blog)=>{
+
+
+let content = "";
+
+if(language === "en"){
+
+content = blog.contentEn;
+
+}else{
+
+content = blog.contentBn;
+
+}
+
+
+
+blogContainer.innerHTML += `
+
+<div class="blog-card">
+
+<img src="${blog.image}" alt="Blog Image">
+
+<h2>${blog.title}</h2>
+
+<p>${blog.summary}</p>
+
+<p>${content}</p>
+
+</div>
+
+`;
+
+
+});
+
+
+}
+
+
+
+banglaBtn.addEventListener("click",()=>{
+
+showBlogs("bn");
+
+});
+
+
+englishBtn.addEventListener("click",()=>{
+
+showBlogs("en");
+
+});
