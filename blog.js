2d88ed1b9fb727f7368d0be7f15b54c07e4385c5
raychulislamrabbi68-prog <raychulislamrabbi68@gmail.com>
2026-08-04 11:46:18 +0600

    Update Firebase configuration in blog.js
    Updated Firebase configuration with new project details.

--- blog.js
+++ blog.js
@@ -2,13 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebas
 import { getDatabase, ref, get } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCirzx_QMyloG6ummoNBtRzhrgxfjcvevs",
+  authDomain: "raychul-islam-rabbi-website.firebaseapp.com",
+  databaseURL: "https://raychul-islam-rabbi-website-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "raychul-islam-rabbi-website",
+  storageBucket: "raychul-islam-rabbi-website.firebasestorage.app",
+  messagingSenderId: "1043773920946",
+  appId: "1:1043773920946:web:750dcb149d29f354cddbab"
 };
 
 const app = initializeApp(firebaseConfig);
